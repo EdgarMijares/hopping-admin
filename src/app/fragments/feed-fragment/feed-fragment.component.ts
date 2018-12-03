c1163f2521aea809efc597e0da11e7d993317e30
Edgar Mijares <progresssystemsoftware@gmail.com>
2018-12-03 10:47:14 -0600

    Fragment feed

---
+++ src/app/fragments/feed-fragment/feed-fragment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-fragment',
+  templateUrl: './feed-fragment.component.html',
+  styles: []
+})
+export class FeedFragmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
