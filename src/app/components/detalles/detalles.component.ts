ce61d138ad8977485a09d7b5571607fa6ccf3132
Edgar Mijares <progresssystemsoftware@gmail.com>
2018-12-03 10:45:10 -0600

    Agregar detalles

--- src/app/components/detalles/detalles.component.ts
+++ src/app/components/detalles/detalles.component.ts
@@ -9,17 +9,8 @@ import { Upload } from '../../models/upload';
 })
 export class DetallesComponent implements OnInit {
 
-    email = new FormControl('', [Validators.required, Validators.email]);
-    lat: number = 24.0291689;
-    lng: number = -104.6606727;
-
     constructor() { }
 
     ngOnInit() { }
 
-    getErrorMessage() {
-        return this.email.hasError('required') ? 'Necesitas ingresar un corre' :
-            this.email.hasError('email') ? 'No es un correo valido' :
-                '';
-    }
 }
