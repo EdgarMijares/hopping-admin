6dbc289385b58889c58105cdafece40e94d131e3
Edgar Mijares <progresssystemsoftware@gmail.com>
2018-12-03 10:52:15 -0600

    Fragment promociones

--- src/app/components/promociones/promociones.component.ts
+++ src/app/components/promociones/promociones.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PromocionesComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() { }
 }
